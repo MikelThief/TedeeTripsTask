e9421860dea0a56334c61efb7897cc26f3d87234
MikelThief <m.bator2@gmail.com>
2022-11-17 17:19:59 +0100

    🚧 Set-up solution structure.

---
+++ TedeeTrips.Application/DI/IServiceCollectionExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace TedeeTrips.Application.DI;
+
+public static class IServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplicationLayer(this IServiceCollection services)
+    {
+        return services;
+    }
+}
