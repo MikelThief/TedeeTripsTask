e045ad66e01fa6b27fdaca37f2d976de01705ac6
MikelThief <m.bator2@gmail.com>
2022-11-20 11:53:49 +0100

    ♻️ Turn imperative processing to functional pipelines.

--- TedeeTrips.Application/Handlers/RegistrationsCommandsHandler.cs
+++ TedeeTrips.Application/Handlers/RegistrationsCommandsHandler.cs
@@ -21,80 +21,50 @@ public class RegistrationsCommandsHandler : IRequestHandler<Register, UnitResult
 
     public async Task<UnitResult<ErrorArray>> Handle(Register request, CancellationToken cancellationToken)
     {
-        var emailAddressResult = EmailAddress.Create(request.EmailAddress);
+        return await EmailAddress
+              .Create(request.EmailAddress)
+              .Bind(async email =>
+              {
+                  Maybe<RegisteredEmailAddress> maybeRegisteredEmailAddress = (await
+                      _registrationsContext.RegisteredEmailAddresses
+                                           .Include(x => x.Enrollments)
+                                           .ThenInclude(x => x.Trip)
+                                           .Where(rea => rea.EmailAddress == email)
+                                           .ToListAsync(cancellationToken)).FirstOrDefault()!;
 
-        if (emailAddressResult.IsFailure)
-        {
-            return emailAddressResult;
-        }
-
-        Maybe<RegisteredEmailAddress> maybeRegisteredEmailAddress = (await
-                _registrationsContext.RegisteredEmailAddresses
-                                     .Include(x => x.Enrollments)
-                                     .ThenInclude(x => x.Trip)
-                                     .ToListAsync(cancellationToken))
-                                     .FirstOrDefault(rea => rea.EmailAddress == emailAddressResult.Value)!;
-
-      
-        await maybeRegisteredEmailAddress.ExecuteNoValue(
-            async () =>
-            {
-                maybeRegisteredEmailAddress = new RegisteredEmailAddress()
-                    {EmailAddress = emailAddressResult.Value};
-                await _registrationsContext.RegisteredEmailAddresses.AddAsync(
-                    maybeRegisteredEmailAddress.GetValueOrThrow(), cancellationToken);
-            });
-
-        var maybeTrip = Maybe.From(await _registrationsContext.Trips.FindAsync(new object?[] { request.TripId }, cancellationToken));
-
-        if (maybeTrip.HasValue)
-        {
-            var enrollResult = maybeRegisteredEmailAddress.GetValueOrThrow().EnrollIn(maybeTrip.GetValueOrThrow()!);
-            
-            if (enrollResult.IsFailure)
-            {
-                return UnitResult.Failure(enrollResult.Error);
-            }
-            else
-            {
-                await _registrationsContext.SaveChangesAsync(cancellationToken);
-                return UnitResult.Success<ErrorArray>();
-            }
-        }
-
-        return Errors.Trip.NotFound().ToErrorArray();
+                  await maybeRegisteredEmailAddress.ExecuteNoValue(async () =>
+                  {
+                      maybeRegisteredEmailAddress = new RegisteredEmailAddress {EmailAddress = email};
+                      await _registrationsContext.RegisteredEmailAddresses.AddAsync(
+                          maybeRegisteredEmailAddress.GetValueOrThrow(), cancellationToken);
+                  });
+                  return maybeRegisteredEmailAddress.ToResult(
+                      Errors.RegisteredEmailAddress.NotRegistered().ToErrorArray());
+              })
+              .Bind(async rea =>
+              {
+                  Maybe<Trip> maybeTrip = (await _registrationsContext.Trips.FindAsync(new object?[] { request.TripId }, cancellationToken))!;
+                  return maybeTrip
+                      .ToResult(Errors.Trip.NotFound().ToErrorArray())
+                      .Map(trip => new { Trip = trip, RegisteredEmailAddress = rea });
+              })
+              .Check(args => args.RegisteredEmailAddress.EnrollIn(args.Trip))
+              .Tap(_ => _registrationsContext.SaveChangesAsync(cancellationToken));
     }
-
-
-    public async Task<UnitResult<ErrorArray>> Handle(Unregister request, CancellationToken cancellationToken)
-    {
-        var emailAddressResult = EmailAddress.Create(request.EmailAddress);
-
-        if (emailAddressResult.IsFailure)
-        {
-            return emailAddressResult;
-        }
-
-        Maybe<RegisteredEmailAddress> maybeRegisteredEmailAddress = (await
-            _registrationsContext.RegisteredEmailAddresses
-                                 .Include(x => x.Enrollments)
-                                 .ThenInclude(x => x.Trip)
-                                 .Where(rea => rea.EmailAddress == emailAddressResult.Value)
-                                 .ToListAsync(cancellationToken)).FirstOrDefault()!;
-
-        if (maybeRegisteredEmailAddress.HasNoValue)
-        {
-            return UnitResult.Failure(Errors.RegisteredEmailAddress.NotRegistered().ToErrorArray());
-        }
-
-        var disenrollResult = maybeRegisteredEmailAddress.GetValueOrThrow().Disenroll(request.TripId);
-
-        if (disenrollResult.IsFailure)
-        {
-            return disenrollResult;
-        }
-        
-        await _registrationsContext.SaveChangesAsync(cancellationToken);
-        return UnitResult.Success<ErrorArray>();
-    }
+    
+    public async Task<UnitResult<ErrorArray>> Handle(Unregister request, CancellationToken cancellationToken) =>
+        await EmailAddress
+              .Create(request.EmailAddress)
+              .Bind(async email =>
+              {
+                  Maybe<RegisteredEmailAddress> maybeRegisteredEmailAddress = (await
+                      _registrationsContext.RegisteredEmailAddresses
+                                           .Include(x => x.Enrollments)
+                                           .ThenInclude(x => x.Trip)
+                                           .Where(rea => rea.EmailAddress == email)
+                                           .ToListAsync(cancellationToken)).FirstOrDefault()!;
+                  return maybeRegisteredEmailAddress.ToResult(Errors.RegisteredEmailAddress.NotRegistered().ToErrorArray());
+              })
+              .Check(rea => rea.Disenroll(request.TripId))
+              .Tap(_ => _registrationsContext.SaveChangesAsync(cancellationToken));
 }
--- TedeeTrips.Domain/Entities/RegisteredEmailAddress.cs
+++ TedeeTrips.Domain/Entities/RegisteredEmailAddress.cs
@@ -13,32 +13,24 @@ public class RegisteredEmailAddress : Entity<Guid>
 
     public UnitResult<ErrorArray> EnrollIn(Trip trip)
     {
-        if (Enrollments.Any(x => x.Trip == trip))
-        {
-            return UnitResult.Failure<ErrorArray>(Errors.Enrollment.UserAlreadyEnrolled());
-        }
-
-        var enrollment = new Enrollment
-        {
-            Trip = trip,
-            RegisteredEmailAddress = this
-        };
+        return UnitResult.FailureIf(Enrollments.Any(x => x.Trip == trip), Errors.Enrollment.UserAlreadyEnrolled().ToErrorArray())
+                         .Tap(() =>
+                         {
+                             var enrollment = new Enrollment
+                             {
+                                 Trip = trip,
+                                 RegisteredEmailAddress = this
+                             };
         
-        _enrollments.Add(enrollment);
-
-        return UnitResult.Success<ErrorArray>();
+                             _enrollments.Add(enrollment);
+                         });
     }
     
     public UnitResult<ErrorArray> Disenroll(Guid tripId)
     {
-        var maybeEnrollment = Maybe.From(_enrollments.FirstOrDefault(x => x.Trip.Id == tripId));
-
-        if (maybeEnrollment.HasNoValue)
-        {
-            return UnitResult.Failure<ErrorArray>(Errors.Enrollment.UserNotEnrolled());
-        }
-
-        _enrollments.Remove(maybeEnrollment.GetValueOrThrow()!);
-        return UnitResult.Success<ErrorArray>();
+        return Maybe
+            .From(_enrollments.FirstOrDefault(x => x.Trip.Id == tripId)!)
+            .ToResult(Errors.Enrollment.UserNotEnrolled().ToErrorArray())
+            .Tap(e => _enrollments.Remove(e));
     }
 }
